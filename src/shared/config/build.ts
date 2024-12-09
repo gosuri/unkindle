166680ca67ccfe5e8fe1b6ca5d0a7a1d59aa2b04
Greg Osuri <me@gregosuri.com>
2024-12-08 22:37:23 -0600

    build: add electron-builder for application packaging
    - Install electron-builder as dev dependency
    - Required for creating distributable packages
    - Fixes build pipeline for macOS distribution
    
    Signed-off-by: Greg Osuri <me@gregosuri.com>

---
+++ src/shared/config/build.ts
@@ -0,0 +1,7 @@
+export const BUILD_CONFIG = {
+    requiredFiles: [
+        'build/icon.icns',  // For macOS
+        'build/icon.ico',   // For Windows
+        'build/icon.png'    // For Linux
+    ]
+}; 
